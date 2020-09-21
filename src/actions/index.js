c734e2c620daa019d07c224188c715f723a2a6cd
Vrishty Garg <vrishty29@gmail.com>
2020-09-21 14:03:31 +1000

    Fisrt commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = "90630f107f923b736c25f84a9c3e8071";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
